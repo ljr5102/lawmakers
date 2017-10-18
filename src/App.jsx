cd3294dab155e490242c956bc2f4ab1973f9ff6f
ljr5102 <ljr5102@gmail.com>
2017-10-18 08:29:09 -0400

    start a simple landing page component with base styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import './Styles/App.scss';
 
 const Dashboard = () => (
   <div>Dashboard!!</div>
@@ -13,13 +14,28 @@ const Dashboard = () => (
 //   });
 // };
 
+const LandingPage = () => (
+  <div>
+    <div className="app-page">
+      <div className="app-rows">
+        <div className="top"></div>
+        <div className="middle">
+          <button className="app-enter">Enter Site Here!</button>
+          </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  </div>
+);
+
 const App = () => (
   <Router>
     <div>
-      <Link to="/dashboard">Dashboard</Link>
-      <Route exact path="/dashboard" component={Dashboard} />
+      <LandingPage />
     </div>
   </Router>
 );
+// <Link to="/dashboard">Dashboard</Link>
+// <Route exact path="/dashboard" component={Dashboard} />
 
 export default App;
