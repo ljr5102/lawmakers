64754ff65b0bb12eb6a1a5ca0b272657b6238987
ljr5102 <ljr5102@gmail.com>
2017-10-17 22:44:58 -0400

    add a basic store configuration

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import Reducer from '../Reducers';
+
+const coolApp = combineReducers(Reducer);
 /* eslint-disable no-underscore-dangle */
 // allow access to file urls in the extension!
-const store = createStore(() => ({ initial: 'here' }), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(coolApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 /* eslint-enable */
 export default store;
