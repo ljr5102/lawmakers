8135665627a453fd26995b417afdceb12ceeb6ff
ljr5102 <ljr5102@gmail.com>
2017-10-21 19:33:17 -0400

    remove commented out code from App

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import './Styles/App.scss';
 import { LandingPage, Dashboard } from './Components';
-//
-// window.makeCall = () => {
-//   fetch("https://theunitedstates.io/congress-legislators/legislators-current.json", { method: 'GET', 'Content-Type': 'application/json' }).then(resp => {
-//     return resp.json()
-//   }).then(json => {
-//     debugger
-//   });
-// };
+
 const App = () => (
   <Router>
     <div>
