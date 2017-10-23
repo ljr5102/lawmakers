bc0302647c69114e8a8e6c85ce136dbf669a7453
ljr5102 <ljr5102@gmail.com>
2017-10-22 21:48:35 -0400

    update the load function in ListedCongress container

--- src/Components/Dashboard/ListedCongress.js
+++ src/Components/Dashboard/ListedCongress.js
@@ -1,16 +1,26 @@
 import { fromJS } from 'immutable';
 import { connect } from 'react-redux';
 import CongressionalList from './CongressionalList';
-import { getCongressmenSuccess } from '../../Actions';
+import { getCongressmenSuccess, getCongressmenFailure,
+  fetchingCongressmenRequest, fetchingCongressmenEnd,
+} from '../../Actions';
+import { fetchCongressmen } from '../../Utils';
+
+const mapStateToProps = ({ congressmen }) => ({
+  congressmen,
+});
 
 const mapDispatchToProps = dispatch => ({
   load: () => {
-    fetch('https://theunitedstates.io/congress-legislators/legislators-current.json', { method: 'GET', 'Content-Type': 'application/json' }).then(resp => (
+    dispatch(fetchingCongressmenRequest());
+    fetchCongressmen().then(resp => (
       resp.json()
     )).then((json) => {
       dispatch(getCongressmenSuccess(fromJS(json)));
-    });
+    }, () => {
+      dispatch(getCongressmenFailure());
+    }).then(() => dispatch(fetchingCongressmenEnd()));
   },
 });
 
-export default connect(null, mapDispatchToProps)(CongressionalList);
+export default connect(mapStateToProps, mapDispatchToProps)(CongressionalList);
