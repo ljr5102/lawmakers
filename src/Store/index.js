497d13dfe96f46271ca6435e0bc13af78663f263
ljr5102 <ljr5102@gmail.com>
2017-10-23 08:18:38 -0400

    update store to handle middleware additions

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,13 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { middleware as reduxPackMiddleware } from 'redux-pack';
 import Reducer from '../Reducers';
 
 /* eslint-disable no-underscore-dangle */
 // allow access to file urls in the extension!
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 const application = combineReducers(Reducer);
-const store = createStore(application, devTools);
+const enhancers = composeEnhancers(applyMiddleware(reduxPackMiddleware));
+const store = createStore(application, enhancers);
 export default store;
