a3e0623ae8354957c3bb1f5a207eb80d2928cd3e
ljr5102 <ljr5102@gmail.com>
2017-10-24 08:15:21 -0400

    create chunk function to throw all members into groups of 4 to display on page

--- src/Components/Dashboard/CongressionalList.jsx
+++ src/Components/Dashboard/CongressionalList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { List } from 'immutable';
 import CongressionalTile from './CongressionalTile';
 
 class CongressionalList extends React.Component {
@@ -7,14 +8,30 @@ class CongressionalList extends React.Component {
     this.props.load();
   }
 
+  chunkMembers() {
+    const list = this.props.congressmen.get('list');
+    return list.reduce((x, y) => {
+      if (x.get(-1).size === 4) {
+        return x.push(List([y]));
+      }
+      const thing = x.get(-1).push(y);
+      return x.set(-1, thing);
+    }, List([List([])]));
+  }
+
+  renderChunks() {
+    return this.chunkMembers().map(grp => (
+      <div className="cl-group">
+        {grp.map(tile => (
+          <CongressionalTile person={tile} />
+        ))}
+      </div>
+    ));
+  }
+
   render() {
     return this.props.congressmen.get('list').size > 0 ? (
-      <div style={{ width: '95%', margin: 'auto' }}>
-        <CongressionalTile person={this.props.congressmen.get('list').get(0)} />
-        <CongressionalTile person={this.props.congressmen.get('list').get(1)} />
-        <CongressionalTile person={this.props.congressmen.get('list').get(2)} />
-        <CongressionalTile person={this.props.congressmen.get('list').get(3)} />
-      </div>
+      this.renderChunks()
     ) : null;
   }
 }
--- src/Components/Dashboard/CongressionalTile.jsx
+++ src/Components/Dashboard/CongressionalTile.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CongressionalTile = ({ person }) => (
-  <div className="ct-group">
+  <div className="ct-group" style={{ border: person.get('party') === 'Democrat' ? '4px solid blue' : '4px solid red' }}>
     <img alt="text" src={`https://theunitedstates.io/images/congress/225x275/${person.get('id')}.jpg`} style={{ margin: 'auto', width: 225, height: 275, background: 'yellow' }} />
     <div style={{ background: 'lightblue' }}>{person.get('name')}</div>
   </div>
