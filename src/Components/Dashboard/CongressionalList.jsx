ba945f1213889e34243e317cdc8a9bcf44479018
ljr5102 <ljr5102@gmail.com>
2017-10-23 22:23:30 -0400

    add tile component into list

--- src/Components/Dashboard/CongressionalList.jsx
+++ src/Components/Dashboard/CongressionalList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CongressionalTile from './CongressionalTile';
 
 class CongressionalList extends React.Component {
   componentWillMount() {
@@ -7,9 +8,14 @@ class CongressionalList extends React.Component {
   }
 
   render() {
-    return (
-      <div>Hello!</div>
-    );
+    return this.props.congressmen.get('list').size > 0 ? (
+      <div style={{ width: '95%', margin: 'auto' }}>
+        <CongressionalTile person={this.props.congressmen.get('list').get(0)} />
+        <CongressionalTile person={this.props.congressmen.get('list').get(1)} />
+        <CongressionalTile person={this.props.congressmen.get('list').get(2)} />
+        <CongressionalTile person={this.props.congressmen.get('list').get(3)} />
+      </div>
+    ) : null;
   }
 }
 
