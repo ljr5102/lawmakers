80c03f82f6d4ecb27a6bce0f748f7ccd6f9c5430
ljr5102 <ljr5102@gmail.com>
2017-10-25 08:13:12 -0400

    make each tile a link and add css classes to them

--- src/Components/Dashboard/CongressionalTile.jsx
+++ src/Components/Dashboard/CongressionalTile.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { Link } from 'react-router-dom';
 
-const CongressionalTile = ({ person }) => (
-  <div className="ct-group">
-    <img alt="text" src={`https://theunitedstates.io/images/congress/225x275/${person.get('id')}.jpg`} style={{ margin: 'auto', width: 225, height: 275, background: 'yellow' }} />
-    <div className="ct-name" style={{ backgroundColor: person.get('party') === 'Democrat' ? '#0560d2' : '#bf0900' }}>{person.get('name')}</div>
-  </div>
-);
+const CongressionalTile = ({ person }) => {
+  const klass = `ct-name ${person.get('party').toLowerCase()}`;
+  return (
+    <Link to={`/member/${person.get('id')}`}>
+      <div className="ct-group">
+        <img alt="text" src={`https://theunitedstates.io/images/congress/225x275/${person.get('id')}.jpg`} />
+        <div className={klass}>{person.get('name')}</div>
+      </div>
+    </Link>
+  );
+};
+
+CongressionalTile.propTypes = {
+  person: ImmutablePropTypes.map.isRequired,
+};
 
 export default CongressionalTile;
