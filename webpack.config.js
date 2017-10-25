72c54ba14df8a4cde66b1f8660c7e300c76a96e5
ljr5102 <ljr5102@gmail.com>
2017-10-24 22:12:30 -0400

    add additional libs to vendor libs array

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var VENDOR_LIBS = ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux', 'immutable'];
+var VENDOR_LIBS = ['react', 'react-dom', 'react-router-dom', 'redux', 'react-redux', 'immutable', 'prop-types', 'redux-pack'];
 var extractSass = new ExtractTextPlugin({
   filename: '[name][chunkhash].css',
 });
