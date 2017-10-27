2f0690d95d13932cd3b87f1d31f292ded819aa19
ljr5102 <ljr5102@gmail.com>
2017-10-27 07:49:01 -0400

    display some basic demographic info and image on detailed member page

--- src/Components/Member/MemberDisplay.jsx
+++ src/Components/Member/MemberDisplay.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 
 class MemberDisplay extends React.Component {
   componentWillMount() {
-
+    this.props.member || this.props.load();
   }
+
   render() {
-    return (
-      <div>hello!</div>
+    const { member } = this.props;
+    return member ? (
+      <div className="mem-disp">
+        <div className="mem-top">
+          {member.get('name')}
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+          <div>
+            Other biographical data
+          </div>
+          <div>
+            <img style={{ width: 450, height: 550 }} alt="text" src={`https://theunitedstates.io/images/congress/450x550/${member.get('id')}.jpg`} />
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div>Loading...</div>
     );
   }
 }
