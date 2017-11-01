4a4d9d161fcc66bb0a3e82ef9747b30409a9ad7d
ljr5102 <ljr5102@gmail.com>
2017-11-01 08:19:18 -0400

    add historical term data table into the member display

--- src/Components/Member/MemberDisplay.jsx
+++ src/Components/Member/MemberDisplay.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Map } from 'immutable';
+
+const chamber = Map({
+  rep: 'House of Representatives',
+  sen: 'Senate',
+});
 
 class MemberDisplay extends React.Component {
   componentWillMount() {
@@ -33,7 +39,7 @@ class MemberDisplay extends React.Component {
               </div>
               <div className="data">
                 <div>
-                  Chamber: {member.get('chamber') === 'rep' ? 'House of Representatives' : 'Senate'}
+                  Chamber: {chamber.get(member.get('chamber'))}
                 </div>
                 <div>
                   State: {member.get('state')}
@@ -49,6 +55,33 @@ class MemberDisplay extends React.Component {
           </div>
           <div className="mem-mid-right">
             <img alt="text" src={`https://theunitedstates.io/images/congress/450x550/${member.get('id')}.jpg`} />
+          </div>
+        </div>
+        <div className="mem-bot">
+          <div className="header">Historical Term Data</div>
+          <div className="data">
+            <table>
+              <tbody>
+                <tr>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Type</th>
+                  <th>Party</th>
+                  <th>State</th>
+                  <th>District</th>
+                </tr>
+                {member.get('terms').map((term, idx) => (
+                  <tr key={idx}>
+                    <td>{term.get('start')}</td>
+                    <td>{term.get('end')}</td>
+                    <td>{term.get('type').toUpperCase()}</td>
+                    <td>{term.get('party')}</td>
+                    <td>{term.get('state')}</td>
+                    <td>{term.get('district')}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
