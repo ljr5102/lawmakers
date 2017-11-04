f988b1485d0aa4cf67906765e12a12282ad4c5c2
ljr5102 <ljr5102@gmail.com>
2017-11-04 16:25:20 -0400

    add images of california and use to test how will look on member detail page

--- src/Components/Member/Biographical.jsx
+++ src/Components/Member/Biographical.jsx
@@ -9,37 +9,7 @@ const chamber = Map({
 const Biographical = ({ member }) => (
   <div className="mem-mid">
     <div className="mem-mid-left">
-      <div>
-        <div className="header">
-          Biographical Data:
-        </div>
-        <div className="data">
-          <div>
-            DOB: {new Date(member.get('birthday')).toLocaleDateString()}
-          </div>
-          <div>
-            Religion: {member.get('religion') || 'N/A'}
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className="header">
-          Congressional Data:
-        </div>
-        <div className="data">
-          <div>
-            Chamber: {chamber.get(member.get('chamber'))}
-          </div>
-          <div>
-            State: {member.get('state')}
-          </div>
-          <div>
-            District: {member.get('district')}
-          </div>
-          <div>
-            Party: {member.get('party')}
-          </div>
-        </div>
+      <div className="california blue">
       </div>
     </div>
     <div className="mem-mid-right">
