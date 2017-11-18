9f068bad4fb2a78db550c693144404731d512ce3
ljr5102 <ljr5102@gmail.com>
2017-11-18 17:31:20 -0500

    add logic in reducer for getting proper zoom

--- src/Reducers/congressional-map.js
+++ src/Reducers/congressional-map.js
@@ -1,7 +1,7 @@
 import { handle } from 'redux-pack';
 import { Map } from 'immutable';
 
-const initialState = Map({ isLoading: false, error: null, lat: 0, lng: 0, zoom: 6 });
+const initialState = Map({ isLoading: true, error: null, lat: 0, lng: 0, zoom: 6 });
 
 const APIHandling = {
   onStart: state => state.set('isLoading', true),
@@ -23,9 +23,12 @@ const APIHandling = {
     const latDiff = maxLat - minLat;
     const cntrLng = minLng + (lngDiff / 2);
     const cntrLat = minLat + (latDiff / 2);
+    const maxDiff = lngDiff > latDiff ? lngDiff : latDiff;
+    const zoom = 4.21144 + (10.7153 / ((1.66413 * (maxDiff ** 0.58058)) + 1))
     console.log('longDiff: ', lngDiff)
     console.log('latDiff: ', latDiff)
-    const update = Map({ lat: cntrLat, lng: cntrLng });
+    console.log('zoom: ', zoom)
+    const update = Map({ lat: cntrLat, lng: cntrLng, zoom: Math.round(zoom) });
     return state.merge(update);
     // max diff of 12 still about 6
     // maxx diff of 8 -> 6
@@ -45,11 +48,13 @@ const congressionalMap = (state = initialState, action) => {
   switch (action.type) {
     case 'GET_CONGRESSIONAL_MAP':
       return handle(state, action, {
-        start: prevState => APIHandling.onStart(prevState),
+        start: () => initialState,
         finish: prevState => APIHandling.onFinish(prevState),
         failure: prevState => APIHandling.onFailure(prevState),
         success: prevState => APIHandling.onSuccess(prevState, action),
       });
+    case 'CLEAR_MAP_DATA':
+      return initialState;
     default:
       return state;
   }
