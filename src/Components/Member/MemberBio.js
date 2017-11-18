9e68c27aa732d04dd54d8956de37f66ac3a5e27f
ljr5102 <ljr5102@gmail.com>
2017-11-18 17:31:54 -0500

    add isLoading prop and function for clearing map data

--- src/Components/Member/MemberBio.js
+++ src/Components/Member/MemberBio.js
@@ -1,15 +1,17 @@
 import { connect } from 'react-redux';
 import Biographical from './Biographical';
-import { getMemberMapData } from '../../Actions';
+import { getMemberMapData, clearMapData } from '../../Actions';
 
 const mapStateToProps = ({ congressionalMap }) => ({
   lng: congressionalMap.get('lng'),
   lat: congressionalMap.get('lat'),
   zoom: congressionalMap.get('zoom'),
+  isLoading: congressionalMap.get('isLoading'),
 });
 
 const mapDispatchToProps = dispatch => ({
   load: code => dispatch(getMemberMapData(code)),
+  clearMapData: () => dispatch(clearMapData()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Biographical);
