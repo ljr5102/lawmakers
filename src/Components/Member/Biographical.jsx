4c55059e8e5d08cff63dea499a634d9df058aaf0
ljr5102 <ljr5102@gmail.com>
2017-11-20 08:12:10 -0500

    remove unused biographical stuff and pull load logic into container

--- src/Components/Member/Biographical.jsx
+++ src/Components/Member/Biographical.jsx
@@ -1,47 +1,15 @@
 import React from 'react';
-import { Map, fromJS } from 'immutable';
 import CongressionalMap from './CongressionalMap';
 
-const chamber = Map({
-  rep: 'House of Representatives',
-  sen: 'Senate',
-});
-
-const convertDistrict = (dist) => {
-  if (dist === undefined) {
-    return null;
-  } else if (dist === 0) {
-    return 'AL';
-  } else if (dist.toString().length < 2) {
-    return `0${dist}`;
-  }
-  return dist.toString();
-};
-
 class Biographical extends React.Component {
-  constructor() {
-    super();
-  }
-
-
   componentWillMount() {
     const { member, load } = this.props;
-    const district = convertDistrict(member.get('district'));
-    const notState = ['AS', 'DC', 'GU', 'MP', 'PR', 'VI'].indexOf(member.get('state')) > -1;
-    if (district && !notState) {
-      load(`${member.get('state')}-${district}`);
-    }
+    load(member);
   }
-
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.member.get('id') !== this.props.member.get('id')) {
-      const district = convertDistrict(nextProps.member.get('district'));
-      const notState = ['AS', 'DC', 'GU', 'MP', 'PR', 'VI'].indexOf(nextProps.member.get('state')) > -1;
-      if (district && !notState) {
-        return this.props.load(`${nextProps.member.get('state')}-${district}`)
-      }
-      return this.props.clearMapData();
+      this.props.load(nextProps.member);
     }
   }
 
@@ -50,14 +18,11 @@ class Biographical extends React.Component {
   }
 
   render() {
-    const { member, lat, lng, zoom, isLoading } = this.props;
-    const district = convertDistrict(member.get('district'));
-    const code = `${member.get('state')}-${district}`;
-    const notState = ['AS', 'DC', 'GU', 'MP', 'PR', 'VI'].indexOf(member.get('state')) > -1;
-    return !isLoading || !district || notState ? (
+    const { member, lat, lng, zoom, isLoading, shouldShowMap, code } = this.props;
+    return !isLoading ? (
       <div className="mem-mid">
         <div className="mem-mid-left">
-          <CongressionalMap lat={lat} lng={lng} code={code} zoom={zoom} />
+          {shouldShowMap ? <CongressionalMap lat={lat} lng={lng} code={code} zoom={zoom} /> : null}
         </div>
         <div className="mem-mid-right">
           <img alt="text" src={`https://theunitedstates.io/images/congress/450x550/${member.get('id')}.jpg`} />
@@ -65,6 +30,6 @@ class Biographical extends React.Component {
       </div>
     ) : null;
   }
-};
+}
 
 export default Biographical;
