427f0d2d0bcf527aa15daba809574106d69079b5
ljr5102 <ljr5102@gmail.com>
2017-11-20 08:10:50 -0500

    update zoom equation and add prop for should show map

--- src/Reducers/congressional-map.js
+++ src/Reducers/congressional-map.js
@@ -1,7 +1,15 @@
 import { handle } from 'redux-pack';
 import { Map } from 'immutable';
 
-const initialState = Map({ isLoading: true, error: null, lat: 0, lng: 0, zoom: 6 });
+const initialState = Map({
+  shouldShowMap: false,
+  isLoading: false,
+  error: null,
+  lat: 0,
+  lng: 0,
+  zoom: 0,
+  code: null,
+});
 
 const APIHandling = {
   onStart: state => state.set('isLoading', true),
@@ -24,17 +32,19 @@ const APIHandling = {
     const cntrLng = minLng + (lngDiff / 2);
     const cntrLat = minLat + (latDiff / 2);
     const maxDiff = lngDiff > latDiff ? lngDiff : latDiff;
-    const zoom = 4.21144 + (10.7153 / ((1.66413 * (maxDiff ** 0.58058)) + 1))
+    // const zoom = 4.21144 + (10.7153 / ((1.66413 * (maxDiff ** 0.58058)) + 1))
+    const zoom = -472.497 + (491.003 / ((0.0211047 * (maxDiff ** 0.168236)) + 1))
     console.log('longDiff: ', lngDiff)
     console.log('latDiff: ', latDiff)
     console.log('zoom: ', zoom)
-    const update = Map({ lat: cntrLat, lng: cntrLng, zoom: Math.round(zoom) });
+    const update = Map({ lat: cntrLat, lng: cntrLng, zoom: Math.round(zoom), code: payload.get('code'), shouldShowMap: true });
     return state.merge(update);
     // max diff of 12 still about 6
     // maxx diff of 8 -> 6
     // max diff of 6 roughly zoom of 6
     // max diff of 2 -> 7, maybe 8
     // max diff of 1.05 -> 8
+    // max diff of 0.86 -> 9
     // max diff of 0.8 about 9
     // max diff of 0.37 -> 10
     // max diff of 0.34 -> 10
@@ -48,13 +58,15 @@ const congressionalMap = (state = initialState, action) => {
   switch (action.type) {
     case 'GET_CONGRESSIONAL_MAP':
       return handle(state, action, {
-        start: () => initialState,
+        start: prevState => APIHandling.onStart(prevState),
         finish: prevState => APIHandling.onFinish(prevState),
         failure: prevState => APIHandling.onFailure(prevState),
         success: prevState => APIHandling.onSuccess(prevState, action),
       });
     case 'CLEAR_MAP_DATA':
       return initialState;
+    case 'HIDE_MAP':
+      return state.merge(Map({ shouldShowMap: false, isLoading: false, code: null }));
     default:
       return state;
   }
