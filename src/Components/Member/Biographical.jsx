2f04e68e46fa7627ff43c3f235862299be582b8b
ljr5102 <ljr5102@gmail.com>
2017-11-23 08:05:29 -0500

    add bottom right thumbnail for image to denote party of member

--- src/Components/Member/Biographical.jsx
+++ src/Components/Member/Biographical.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Map } from 'immutable';
 import CongressionalMap from './CongressionalMap';
+
+const partyToImage = Map({
+  Republican: 'elephant',
+  Democrat: 'donkey',
+});
 
 class Biographical extends React.Component {
   componentWillMount() {
@@ -25,6 +31,7 @@ class Biographical extends React.Component {
           {shouldShowMap ? <CongressionalMap lat={lat} lng={lng} code={code} zoom={zoom} /> : null}
         </div>
         <div className="mem-mid-right">
+          <div className={partyToImage.get(member.get('party'))} />
           <img alt="text" src={`https://theunitedstates.io/images/congress/450x550/${member.get('id')}.jpg`} />
         </div>
       </div>
