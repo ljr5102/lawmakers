010eb15792af8a275de5b71d4fbda0c4f0d13090
ljr5102 <ljr5102@gmail.com>
2018-01-27 15:56:15 -0500

    create a filters component and add in a simple text input for searching

---
+++ src/Components/Filters/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Filters = () => (
+  <div className="filter-group">
+    <input
+      type="text"
+      className="filter-search"
+      placeholder="Search By Name"
+    />
+  </div>
+);
+
+export default Filters;
