b564319a344d2474fede5d864deb1d54f6320fed
ljr5102 <ljr5102@gmail.com>
2018-03-23 09:45:13 -0400

    create an AddressAutoComplete component that will handle changes in address and update the filters

---
+++ src/Components/Filters/AddressAutoComplete.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddressAutoComplete extends React.Component {
+  constructor() {
+    super()
+    this.state = { addressEntered: false, address: '' };
+
+    this.clearAddressFilter = this.clearAddressFilter.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.autoComplete = new google.maps.places.Autocomplete(document.getElementById("searchTextField"), { componentRestrictions: { country: 'us' } });
+    this.autoComplete.addListener('place_changed', () => {
+      const address = this.autoComplete.getPlace();
+      this.setState({ addressEntered: true, address: address.formatted_address });
+      this.props.fetchAddressInfo(address);
+    })
+    window.autoComplete = this.autoComplete
+  }
+
+  clearAddressFilter() {
+    this.props.clearAddressFilter();
+    this.setState({ addressEntered: false, address: '' });
+  }
+
+  handleChange(e) {
+    this.setState({ address: e.target.value });
+  }
+
+  render() {
+    return (
+      <div style={{ position: 'relative' }}>
+        <input
+          type="text"
+          name="address"
+          id="searchTextField"
+          className={`filter-search ${this.state.addressEntered ? 'disabled' : ''}`}
+          disabled={this.state.addressEntered}
+          value={this.state.address}
+          onChange={this.handleChange}
+        />
+        {this.state.addressEntered ? (
+          <div className="cancel-filter" onClick={this.clearAddressFilter}>
+            <span>X</span>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default AddressAutoComplete;
