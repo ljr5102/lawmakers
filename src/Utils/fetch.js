18b90ac8a677dc01e9484a7e9addc4ea2320705d
ljr5102 <ljr5102@gmail.com>
2019-02-18 16:52:16 -0500

    add new links for fetching geoJson data from the districts api directly

--- src/Utils/fetch.js
+++ src/Utils/fetch.js
@@ -3,6 +3,8 @@ import { fromJS } from 'immutable';
 const congressAPILink = 'https://theunitedstates.io/congress-legislators/legislators-current.json';
 const fusionAPILink = 'https://www.googleapis.com/fusiontables/v2/';
 const civicAPILink = 'https://www.googleapis.com/civicinfo/v2/representatives/';
+const congressAPILinkNew = code => `https://theunitedstates.io/districts/cds/${code.includes('PA') ? 2018 : 2016}/${code}/shape.geojson`;
+const senateAPILinkNew = state => `https://theunitedstates.io/districts/states/${state}/shape.geojson`;
 
 const fetchCongressAPI = () => (
   fetch(congressAPILink, { method: 'GET', 'Content-Type': 'application/json' })
@@ -15,12 +17,20 @@ const fetchCongressionalMapDataAPI = (code) => {
   return fetch(fusionAPILink + param + googApiKey, { method: 'GET', 'Content-Type': 'application/json' });
 };
 
+const fetchCongressionalMapDataAPINew = code => (
+  fetch(congressAPILinkNew(code), { method: 'GET', 'Content-Type': 'application/json' })
+);
+
 const fetchSenateMapDataAPI = (state) => {
   const rowFetchSql = `SELECT ROWID, Geometries from 1VIlPVQuZyyUaT4j5X_KqGEmuZysxMaW2kshvLZ4V where Name = '${state}';`;
   const param = `query?sql=${rowFetchSql}`;
   const googApiKey = '&key=AIzaSyD8SMlcDgmqT3zUusiEpCZFKB4E0N9SiOk';
   return fetch(fusionAPILink + param + googApiKey, { method: 'GET', 'Content-Type': 'application/json' });
 };
+
+const fetchSenateMapDataAPINew = state => (
+  fetch(senateAPILinkNew(state), { method: 'GET', 'Content-Type': 'application/json' })
+);
 
 const fetchAddressDataAPI = (place) => {
   const { formatted_address } = place;
@@ -39,7 +49,17 @@ const fetchCongressionalMapData = code => fetchCongressionalMapDataAPI(code).the
   err => Promise.reject(err),
 ).then(json => fromJS({ ...json, code }));
 
+const fetchCongressionalMapDataNew = code => fetchCongressionalMapDataAPINew(code).then(
+  resp => resp.json(),
+  err => Promise.reject(err),
+).then(json => fromJS({ ...json, code }));
+
 const fetchSenateMapData = state => fetchSenateMapDataAPI(state).then(
+  resp => resp.json(),
+  err => Promise.reject(err),
+).then(json => fromJS({ ...json, state }));
+
+const fetchSenateMapDataNew = state => fetchSenateMapDataAPINew(state).then(
   resp => resp.json(),
   err => Promise.reject(err),
 ).then(json => fromJS({ ...json, state }));
@@ -52,6 +72,8 @@ const fetchStateDistrictData = place => fetchAddressDataAPI(place).then(
 export {
   fetchCongress,
   fetchCongressionalMapData,
+  fetchCongressionalMapDataNew,
   fetchSenateMapData,
+  fetchSenateMapDataNew,
   fetchStateDistrictData,
 };
