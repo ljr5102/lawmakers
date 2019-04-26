5c2d9efb3753925e00a594a49c352c2c47df74f0
ljr5102 <ljr5102@gmail.com>
2019-04-26 17:17:38 -0400

    eslint fix for dashboard spec

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,13 +1,13 @@
 Cypress.on('window:before:load', (win) => {
   delete win.fetch
-})
+});
 
 describe('Dashboard', () => {
   it('successfully loads', () => {
-    cy.server()
-    cy.route('GET', 'https://theunitedstates.io/congress-legislators/legislators-current.json', [])
-    cy.visit('/')
-    // cy.get('Dashboard').should('be.visible')
-    cy.get('#searchTextField').should('have.attr', 'placeholder', 'Search By Address')
-  })
-})
+    cy.server();
+    cy.route('GET', 'https://theunitedstates.io/congress-legislators/legislators-current.json', []);
+    cy.visit('/');
+    cy.contains('Dashboard');
+    cy.get('#searchTextField').should('have.attr', 'placeholder', 'Search By Address');
+  });
+});
