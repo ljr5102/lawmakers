ee01f49464557dba1afc1b47b2e7daa73d0b4c5b
ljr5102 <ljr5102@gmail.com>
2019-05-12 12:52:54 -0400

    replace img tag in CongressionalTile with CongressionalImage component

--- src/Components/Congress/CongressionalTile.jsx
+++ src/Components/Congress/CongressionalTile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Link } from 'react-router-dom';
+import CongressionalImage from './CongressionalImage';
 
 const CongressionalTile = ({ person }) => {
   const klass = `ct-name ${person.get('party').toLowerCase()}`;
@@ -9,7 +10,7 @@ const CongressionalTile = ({ person }) => {
   return (
     <Link to={`/member/${person.get('id')}`}>
       <div className="ct-group">
-        <img alt="text" src={`https://theunitedstates.io/images/congress/225x275/${person.get('id')}.jpg`} />
+        <CongressionalImage memberId={person.get('id')} />
         <div className={klass}>{person.get('name')}</div>
       </div>
     </Link>
