6c798d15ba1dc879c28a243756acc2b761132296
ljr5102 <ljr5102@gmail.com>
2019-05-12 13:54:43 -0400

    add logic for fallback images if an image is not available from unitedstates.io page

--- src/Components/Congress/CongressionalImage.jsx
+++ src/Components/Congress/CongressionalImage.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class CongressionalImage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageSources: [
+        `https://theunitedstates.io/images/congress/225x275/${props.memberId}.jpg`,
+        `http://bioguide.congress.gov/bioguide/photo/${props.memberId[0]}/${props.memberId}.jpg`,
+      ],
+      imgSrcIndex: 0,
+    };
+    this.handleError = this.handleError.bind(this);
+  }
+
+  handleError() {
+    if (this.state.imgSrcIndex < this.state.imageSources.length) {
+      this.setState({ imgSrcIndex: this.state.imgSrcIndex + 1 });
+    }
   }
 
   render() {
     const { memberId } = this.props;
+    const src = this.state.imageSources[this.state.imgSrcIndex];
     return (
-      <img alt="text" src={`https://theunitedstates.io/images/congress/225x275/${memberId}.jpg`} />
+      <img id={`${memberId}-img`} alt="text" onError={this.handleError} src={src} />
     );
   }
 }
