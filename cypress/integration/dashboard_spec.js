91e6fb78000ad3430a980b779983efdfc3bf4f12
ljr5102 <ljr5102@gmail.com>
2019-05-11 20:55:20 -0400

    add additional dashboard specs

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -1,9 +1,27 @@
 describe('Dashboard', () => {
   it('successfully loads', () => {
     cy.server();
-    cy.route('GET', 'https://theunitedstates.io/congress-legislators/legislators-current.json', []);
+    cy.route('GET', 'https://theunitedstates.io/congress-legislators/legislators-current.json', 'fixture:legislators.json');
     cy.visit('/');
     cy.contains('Dashboard');
-    cy.get('#searchTextField').should('have.attr', 'placeholder', 'Search By Address');
+    cy.get('#address-search').should('have.attr', 'placeholder', 'Search By Address');
+    cy.get('#name-search').should('have.attr', 'placeholder', 'Search By Name');
+  });
+
+  it('shows members that match name entered in search', () => {
+    cy.server();
+    cy.route('GET', 'https://theunitedstates.io/congress-legislators/legislators-current.json', 'fixture:legislators.json');
+    cy.visit('/');
+    cy.get('#name-search').type('sherrod');
+    cy.contains('Sherrod Brown').and('be.visible');
+    cy.get('.ct-name').should('exist');
+  });
+
+  it('filters out members who do not match name entered in search', () => {
+    cy.server();
+    cy.route('GET', 'https://theunitedstates.io/congress-legislators/legislators-current.json', 'fixture:legislators.json');
+    cy.visit('/');
+    cy.get('#name-search').type('bob');
+    cy.get('.ct-name').should('not.exist');
   });
 });
