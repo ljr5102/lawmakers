fffe6f748b19b519d6de983fa39b62f404676fd2
ljr5102 <ljr5102@gmail.com>
2019-05-19 13:54:44 -0400

    have house and senate seal images be loaded in scss file so they get bundled correctly

--- src/Components/Congress/CongressionalImage.jsx
+++ src/Components/Congress/CongressionalImage.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const chamberToDefaultImage = {
-  rep: "url('/assets/house_seal.png')",
-  sen: "url('/assets/senate_seal.png')",
-};
-
 class CongressionalImage extends React.Component {
   constructor(props) {
     super(props);
@@ -33,7 +28,7 @@ class CongressionalImage extends React.Component {
     const { imageSources, imgSrcIndex } = this.state;
     const src = imageSources[imgSrcIndex];
     return imgSrcIndex >= imageSources.length ? (
-      <div className="faux-image" style={{ backgroundImage: chamberToDefaultImage[chamber] }} />
+      <div className={`faux-image ${chamber}`} />
     ) : (
       <img id={`${memberId}-img`} alt="text" onError={this.handleError} src={src} />
     );
